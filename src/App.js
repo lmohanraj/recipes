d5b70825c12f7efbb9f6b1dd13c7270c11e8ad0a
lmohanraj <lmohanraj1997@gmail.com>
2021-12-11 15:59:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+let recipes = [
+  {name : "Roasted Vegetables", time:"60 minutes", url : "https://www.acouplecooks.com/wp-content/uploads/2019/11/Roasted-Vegetables-012-735x919.jpg"},
+  {name : "Sauteed Vegetables", time:"30 minutes", url : "https://www.acouplecooks.com/wp-content/uploads/2020/10/Sauteed-Vegetables-005-735x919.jpg"},
+  {name : "Stir Fry Vegetables", time:"45 minutes", url : "https://www.acouplecooks.com/wp-content/uploads/2020/01/Broccoli-Stir-Fry-011-735x1102.jpg"},
+  {name : "Roasted Broccoli", time:"40 minutes", url : "https://www.acouplecooks.com/wp-content/uploads/2020/03/Roasted-Broccoli-004-735x919.jpg"}
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Our Recipes</h1>
+        <div className='container'>
+        {recipes.map( (recipe) => {
+          return(
+            <div>
+              <img className='image' src={recipe.url} alt={recipe.name} />
+              <h4>{recipe.name}</h4>
+              <h6>Time: <span>{recipe.time}</span></h6>
+            </div>
+          );
+        })}
+        </div>
     </div>
   );
 }
